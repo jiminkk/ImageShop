e70cdffddefb3c9dbb16a482074246539efd0c06
Calvin Yin <calvin_yin@umail.ucsb.edu>
2015-04-04 17:34:06 -0700

    fixed some shit eg display

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+
+
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 from flask import render_template
+from PIL import Image
 
 
-UPLOAD_FOLDER = '/Users/Calvin_Yin/Desktop'
+UPLOAD_FOLDER = os.getcwd()+"/input/"
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
@@ -14,6 +17,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           
 
 @app.route('/')
 def home(): 
@@ -25,8 +29,14 @@ def upload_file():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            data = "I've done all the things, and here is a new photo" 
+            try:
+                os.stat(UPLOAD_FOLDER)
+            except:
+                os.mkdir(UPLOAD_FOLDER)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], "User_image." + file.filename.rsplit('.',1)[1]))
+            data = "/static/input/User_image."+file.filename.rsplit('.',1)[1]
+            #im = Image.open(os.getcwd()+"/input/User_image."+file.filename.rsplit('.',1)[1]
+            #data = im
             return render_template('test.html', data = data) 
             #return redirect(url_for('uploaded_file',
              #             	filename=filename))
